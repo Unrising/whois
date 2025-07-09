12df321205ea529e9ff5a17bdc6af341ddc3fd24
Michael <michael.picard14@gmail.com>
2025-07-09 21:35:50 +0200

    feat: Created data branch + machines.js

---
+++ backend/data/machines.js
@@ -0,0 +1,8 @@
+let machines = [
+    {id: 1, name: 'TI-84', user: null},
+    {id: 2, name: 'Casio FX', user: null},
+    {id: 3, name: 'HP Prime', user: null},
+]
+
+module.exports = { machines };
+
