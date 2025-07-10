e1f59fd41c9191fdf38de798e55d2d9210f456d6
Michael <michael.picard14@gmail.com>
2025-07-10 22:01:37 +0200

    feat: controllers and routes

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
+const machinesRoutes = require('./routes/machinesRoutes');
 
-app.get('/', (req,res) => {
-    res.send('Backend is running');
-});
+app.use(express.json());
+app.use('/api', machineRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
+    console.log(`Server is running on http://localhost${PORT}`);
+});
+
+
